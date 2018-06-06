bcbea06ef63b10c1d5fbc236b15f8321737a1e56
misepp <mikko.seppanen123@gmail.com>
2018-06-06 22:27:04 +0300

    SQLite database write and read made functional

--- actionlibprivate.cpp
+++ actionlibprivate.cpp
@@ -2,7 +2,12 @@
 
 #include <QSqlDatabase>
 #include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
 #include <QObject>
+#include <QVariant>
+#include <QList>
+#include <QDir>
 
 
 ActionLibPrivate::ActionLibPrivate()
@@ -13,33 +18,66 @@ ActionLibPrivate::ActionLibPrivate()
 bool ActionLibPrivate::createDBConnection()
 {
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("databaseFile.db");
-    db.setConnectOptions("QSQLITE_ENABLE_SHARED_CACHE = 1");
+    db.setDatabaseName(QDir::currentPath() + "//databaseFile.db");
+    qDebug() << "Curent Path: " << QDir::currentPath();
+    db.setConnectOptions("QSQLITE_ENABLE_SHARED_CACHE");
+//    db.setConnectOptions("QSQLITE_ENABLE_SHARED_CACHE = 1");
     if (!db.open())
     {
         return false;
     }
 
     QSqlQuery query;
-    return query.exec("create table results (firstname varchar(20), points int)");
+    //bool temp = query.prepare("create table results (id int primary key, firstname varchar(20), points int)");
+    return query.exec("create table results (firstname varchar(50), points int)");
 }
 
-bool ActionLibPrivate::InsertDBData(QString name, int points)
+bool ActionLibPrivate::insertDBData(QString name, int points)
 {
     if(QSqlDatabase::contains())
     {
        if(QSqlDatabase::database().isOpen())
        {
            QSqlQuery query;
-           QString str = "insert into results values ('name', points)";
-           QString temp = str.replace(str.indexOf("name"), QString("name").length(), name);
-           temp = temp.replace(temp.indexOf("points"), QString("points").length(), QString::number(points));
-           return query.exec(temp);
-           //return query.exec("insert into results values (" + "\"" + name + "\"" + ", " + points + ")");
+           QString str = "insert into results values "; // (name, points)";
+//           query.prepare("insert into results values (:name, :points)");
+//           query.bindValue(":name", name);
+//           query.bindValue(":points", points);
+           qDebug() << query.lastError();
+           str = str + "(" + "\"" + name + "\", " + QString::number(points) + ")";
+//           QString temp = str.replace(str.indexOf("name"), QString(name).length() + 1, name + "'");
+//           temp = temp.replace(temp.indexOf("points"), QString::number(points).length(), QString::number(points));
+//           return query.exec();
+           return query.exec(str);
        }
     }
     return false;
+}
 
+QVariant ActionLibPrivate::readDBData(QString name)
+{
+    if(QSqlDatabase::contains())
+    {
+        if(QSqlDatabase::database().isOpen())
+        {
+            QList<QVariant> result;
+            QVariant data;
+            QSqlQuery query;
+            QString str = "SELECT firstname, points FROM results WHERE firstname="; //':name'";
+//            QString temp = str.replace(str.indexOf(":name"), QString(name).length(), name);
+            str = str + "\"" + name + "\"";
+//            query.prepare("SELECT * FROM results WHERE firstname='name'");
+            //query.bindValue("name", name);
+            query.exec(str);
+            qDebug() << query.lastError();
+            while(query.next()) {
+                result.append(QString(query.value(0).toString() + "," + query.value(1).toString()));
+            }
+            data.setValue<QList<QVariant>>(result);
+            return data;
+        }
+    }
+    return QList<QVariant>();
 }
 
 void ActionLibPrivate::CloseDB()
